67964fb1d1ea1c55fa66292a7932c09311cc867e
Geeth S Gunnampalli <geeth.gunnampalli@hotmail.com>
2021-05-13 02:02:14 -0500

    Initial Commit

---
+++ AnimeQuote/AnimeQuoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeQuoteApp.swift
+//  AnimeQuote
+//
+//  Created by Geeth Gunnampalli on 5/13/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimeQuoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
